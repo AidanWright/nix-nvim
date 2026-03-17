61e189862f84c3836964154a1ca8418d4f62a2d6
Aidan Wright <38870143+AidanWright@users.noreply.github.com>
2026-03-17 15:16:22 -0400

    hide short codes

--- nvim/after/plugin/emojify.lua
+++ nvim/after/plugin/emojify.lua
@@ -2,4 +2,14 @@ require("emojify").setup({
   inlay = true,
 })
 
+-- Hide short code
+vim.api.nvim_create_autocmd("FileType", {
+  pattern = "markdown",
+  callback = function()
+    vim.fn.matchadd("Conceal", ":[a-z_]*:", 10, -1, { conceal = "" })
+    vim.opt_local.conceallevel = 2
+    vim.opt_local.concealcursor = "nc"
+  end,
+})
+
 vim.opt.conceallevel = 2
