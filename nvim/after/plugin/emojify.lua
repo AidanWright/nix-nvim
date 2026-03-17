74d5ebab01422e5e00183041175c157ac50d7952
Aidan Wright <38870143+AidanWright@users.noreply.github.com>
2026-03-17 14:57:01 -0400

    add delay

--- nvim/after/plugin/emojify.lua
+++ nvim/after/plugin/emojify.lua
@@ -2,4 +2,13 @@ require("emojify").setup({
   inlay = false,
 })
 
+vim.api.nvim_create_autocmd("FileType", {
+  pattern = "markdown",
+  callback = function()
+    vim.defer_fn(function()
+      require("emojify").enable()
+    end, 500)
+  end,
+})
+
 vim.opt.conceallevel = 2
