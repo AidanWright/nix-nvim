a1c33ed18365cd34d07eec85c636e93116f2c64d
Aidan Wright <38870143+AidanWright@users.noreply.github.com>
2026-03-17 15:10:49 -0400

    fix cursor

--- nvim/after/plugin/emojify.lua
+++ nvim/after/plugin/emojify.lua
@@ -1,14 +1,5 @@
 require("emojify").setup({
-  inlay = false,
-})
-
-vim.api.nvim_create_autocmd("FileType", {
-  pattern = "markdown",
-  callback = function()
-    vim.defer_fn(function()
-      require("emojify").enable_emojify()
-    end, 500)
-  end,
+  inlay = true,
 })
 
 vim.opt.conceallevel = 2
