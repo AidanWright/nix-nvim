2b78a010d477de71917bb29cd27816410317387c
Aidan Wright <38870143+AidanWright@users.noreply.github.com>
2026-03-17 15:11:58 -0400

    disable both glpyhs showing

--- nvim/after/plugin/emojify.lua
+++ nvim/after/plugin/emojify.lua
@@ -1,5 +1,12 @@
 require("emojify").setup({
-  inlay = true,
+  inlay = false,
+})
+
+vim.api.nvim_create_autocmd("FileType", {
+  pattern = "markdown",
+  callback = function()
+    vim.opt_local.concealcursor = "nc"
+  end,
 })
 
 vim.opt.conceallevel = 2
